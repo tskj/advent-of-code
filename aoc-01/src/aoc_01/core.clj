7ecf5b7121a986d08fa8be22276551b245492c05
Tarjei <epost@tarjei.org>
2021-12-05 21:51:01 +0100

    Day 1 part 2

--- aoc-01/src/aoc_01/core.clj
+++ aoc-01/src/aoc_01/core.clj
@@ -10,18 +10,24 @@
 (def list-of-nums
   (map #(Integer/parseInt %) list-of-str-nums))
 
-(defn slide [list]
-  (if (< (count list) 2)
-    '()
-    (let [[x-1 x-2 & xs] list
-          rest (conj xs x-2)]
-      (conj
-       (slide rest)
-       [x-1 x-2])))) 
+(defn window [n list]
+  (loop [input (seq list)
+         output []]
+    (if (< (count input) n)
+      output
+      (recur 
+       (rest input) 
+       (conj output (take n input))))))
 
-(slide [1 2 3])
-(slide [1 2 3 4])
+(defn slide [list]
+  (window 2 list))
+
 (slide [])
+(slide [1])
+(slide [1 2])
+(slide [1 2 3])
+(window 3 [1 2 3 4])
+(window 3 [1 2 3 4 5])
 (slide list-of-nums)
 
 (def example-data
@@ -37,10 +43,13 @@
    263])
 
 (defn calc-result [data]
-  (->> (slide data)  
-    (map (fn [[x y]] (- y x)))
-    (filter pos-int?)
-    (count)))
+  (->> data
+       (window 3)
+       (map #(reduce + %))
+       (slide)
+       (map (fn [[x y]] (- y x)))
+       (filter pos-int?)
+       (count)))
 
 (calc-result example-data)
 (calc-result list-of-nums)
