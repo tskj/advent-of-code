9042d9e4b9161ee51a04ecc0a625c8392b8f0122
Tarjei <epost@tarjei.org>
2021-12-05 22:26:48 +0100

    Day 2 part 1

--- aoc-01/src/aoc_01/core.clj
+++ aoc-01/src/aoc_01/core.clj
@@ -2,54 +2,40 @@
   (:gen-class)
   (:require [clojure.string :as str]))
 
-(def text (slurp "./data/input.txt"))
-
-(def list-of-str-nums
-  (str/split-lines text))
-
-(def list-of-nums
-  (map #(Integer/parseInt %) list-of-str-nums))
-
-(defn window [n list]
-  (loop [input (seq list)
-         output []]
-    (if (< (count input) n)
-      output
-      (recur 
-       (rest input) 
-       (conj output (take n input))))))
-
-(defn slide [list]
-  (window 2 list))
-
-(slide [])
-(slide [1])
-(slide [1 2])
-(slide [1 2 3])
-(window 3 [1 2 3 4])
-(window 3 [1 2 3 4 5])
-(slide list-of-nums)
+(def data 
+  (->> "./data/input.txt"
+   (slurp)
+   (str/split-lines)
+   (map #(str/split % #" "))
+   (map (fn [[dir amount]] [dir (Integer/parseInt amount)]))))
 
 (def example-data
-  [199
-   200
-   208
-   210
-   200
-   207
-   240
-   269
-   260
-   263])
+  [["forward" 5]
+   ["down" 5]
+   ["forward" 8]
+   ["up" 3]
+   ["down" 8]
+   ["forward" 2]])
 
 (defn calc-result [data]
-  (->> data
-       (window 3)
-       (map #(reduce + %))
-       (slide)
-       (map (fn [[x y]] (- y x)))
-       (filter pos-int?)
-       (count)))
+  (loop [input data
+         x 0
+         depth 0]         
+    (if (empty? input)
+      {:x x
+       :depth depth}       
+      (let [[dir amount] (first input)]
+        (recur
+         (rest input)       
+         (if (= dir "forward") 
+           (+ x amount) 
+           x)
+         (if (= dir "down")
+           (+ depth amount)
+           (if (= dir "up")
+             (- depth amount)
+             depth)))))))
 
 (calc-result example-data)
-(calc-result list-of-nums)
+(def res (calc-result data))
+(* (res :x) (res :depth))
