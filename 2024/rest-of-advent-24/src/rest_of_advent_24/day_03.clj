77638dea1ce00ebfc2b9861c73b2c02d8c693f18
tskj <epost@tarjei.org>
2024-12-05 01:52:16 +0100

    okay it works now

--- 2024/rest-of-advent-24/src/rest_of_advent_24/day_03.clj
+++ 2024/rest-of-advent-24/src/rest_of_advent_24/day_03.clj
@@ -5,34 +5,31 @@
 (defmacro blk
   {:clj-kondo/ignore true}
   ([] nil)
-  ([& block]
-   (assert (seq? block))
-   (assert (> (count block) 0))
-   (let [line-of-code (first block)
-         rest-of-codeblock (rest block)]
+  ([last-return] last-return)
+  ([line-of-code & rest-of-codeblock]
+   (cond (and (seq? line-of-code) (= 'const (first line-of-code))) ;; const
+         (do (assert (= 3 (count line-of-code)))
+             (let [[_ lhs rhs] line-of-code]
+               `(let [rhs# ~rhs
+                      ~lhs rhs#]
+                 (blk ~@rest-of-codeblock))))
 
-     (cond (and (seq? line-of-code) (= 'const (first line-of-code))) ;; const
-           (do (assert (= 3 (count line-of-code)))
-               (let [[_ lhs rhs] line-of-code]
-                 `(let [~lhs ~rhs]
-                   (blk ~@rest-of-codeblock))))
+         (and (seq? line-of-code) (= 'const-try (first line-of-code)))
+         (do (assert (= 3 (count line-of-code)))
+             (let [[_ lhs rhs] line-of-code]
+               `(let [rhs# ~rhs]
+                  (if (nil? rhs#)
+                    nil
+                    (let [~lhs rhs#]
+                        (blk ~@rest-of-codeblock))))))
 
-           (and (seq? line-of-code) (= 'const-try (first line-of-code)))
-           (do (assert (= 3 (count line-of-code)))
-               (let [[_ lhs rhs] line-of-code]
-                 `(let [rhs# ~rhs]
-                    (if (nil? rhs#)
-                      nil
-                      (let [~lhs rhs#]
-                          (blk ~@rest-of-codeblock))))))
-
-           :else ;; return first non-nil value
-           `(let [result# ~line-of-code]
-             (if (nil? result#)
-               (blk ~@rest-of-codeblock)
-               (if (and (map? result#) (= (set (keys result#)) #{:return}))
-                (:return result#)
-                result#)))))))
+         :else ;; return first non-nil value
+         `(let [result# ~line-of-code]
+           (if (nil? result#)
+             (blk ~@rest-of-codeblock)
+             (if (and (map? result#) (= (set (keys result#)) #{:return}))
+              (:return result#)
+              result#))))))
 
 (let [result__6860__auto__ "test"]
   (println result__6860__auto__)
@@ -64,27 +61,10 @@
      (if false "return this? no")
      a)
 
-(def input
-  (->> "resources/day-03-input.txt"
-       (slurp)))
+(def input (slurp "resources/day-03-input.txt"))
 
 (defn is-digit? [d]
   (Character/isDigit d))
-
-(macroexpand
-  '(blk
-     (const-try [next-digits s'] (parse-n-digit-int (subs s 1)))
-     (if (empty? s) {:return nil})
-     (const first-character (first s))
-     (if (not (is-digit first-character)) {:return nil})
-
-     (println "parse-n called with " s " and first-character=" first-character)
-
-     (const digit first-character)
-     (const-try [next-digits s'] (parse-n-digit-int (subs s 1)))
-     (println "next-digits " next-digits " and s'=" s')
-
-     [(str digit (or next-digits "")) s']))
 
 (defn parse-n-digit-int [s]
   (blk
@@ -99,79 +79,68 @@
    (const [next-digits s'] (or (parse-n-digit-int (subs s 1))
                                [nil r]))
 
-   {:return [(str digit (or next-digits "")) s']}))
+   [(str digit (or next-digits "")) s']))
 
 (defn str-to-int [s]
   (assert (every? is-digit? s) "this function only applies to int digits")
-  (reduce (fn [acc x]
-            (let [digit (- (int x) 48)]
-              (+ digit (* 10 acc))))
-    0 s))
+  (->> [0 s]
+       (apply reduce (fn [acc x]
+                       (let [digit (- (int x) 48)]
+                         (+ digit (* 10 acc)))))))
+
+(defn parse-literal [p s]
+  (assert (not (nil? s)) "string can't be null")
+  (if (starts-with? s p)
+    [p (subs s (count p))]))
 
 (defn parse-comma [s]
-  (if (= \, (first s))
-    ["," (subs s 1)]
-    [nil s]))
+  (assert (not (nil? s)) "comma s is nil")
+  (parse-literal "," s))
 
 (defn parse-ending-paren [s]
-  (if (= \) (first s))
-    [")" (subs s 1)]
-    [nil s]))
+  (assert (not (nil? s)) "end-paren s is nil")
+  (parse-literal ")" s))
 
 (defn parse-do [s]
-  (if (starts-with? s "do()")
-    ["do()" (subs s 4)]
-    [nil s]))
+  (assert (not (nil? s)) "do s is nil")
+  (parse-literal "do()" s))
 
 (defn parse-don't [s]
-  (if (starts-with? s "don't()")
-    ["don't()" (subs s 7)]
-    [nil s]))
+  (assert (not (nil? s)) "don't s is nil")
+  (parse-literal "don't()" s))
+
+(def mul "mul(") ;; )
 
 (defn parse-mul-thing [initial-s]
-  (cond (< (count initial-s) 4)
-        [nil initial-s]
+  (blk
+    (if (< (count initial-s) 4) {:return nil}) ;; TODO: remove????
 
-        (not (starts-with? initial-s "mul("))
-        [nil initial-s]
+    (const-try [_mul s]     (parse-literal mul initial-s))
+    (const-try [the-int s]  (parse-n-digit-int s))
+    (const-try [_comma s] (parse-comma s))
+    (const-try [the-next-int s] (parse-n-digit-int s))
+    (const-try [_end-paren s]   (parse-ending-paren s))
 
-        :else
-        (let [s (subs initial-s 4)
-              [the-int s] (parse-n-digit-int s)]
-
-          (cond (nil? the-int)
-                [nil initial-s]
-
-            :else
-            (let [[_comma s] (parse-comma s)]
-              (cond (nil? _comma)
-                    [nil initial-s]
-
-               :else
-               (let [[the-next-int s] (parse-n-digit-int s)]
-                 (cond (nil? the-next-int)
-                       [nil initial-s]
-
-                   :else
-                   (let [[_paren s] (parse-ending-paren s)]
-                     (if (nil? _paren)
-                       [nil initial-s]
-                       [(* (str-to-int the-int) (str-to-int the-next-int)) s]))))))))))
+    [(* (str-to-int the-int) (str-to-int the-next-int)) s]))
 
 (->>
 
   (loop [acc []
          enabled true
          s input]
+
     (if (empty? s)
       acc
       (if enabled
-        (let [[result s] (parse-mul-thing s)
-              [d s] (parse-don't s)]
+        (let [[result s] (or (parse-mul-thing s)
+                             [nil s])
+              [d s] (or (parse-don't s)
+                        [nil s])]
           (if (nil? result)
             (recur acc (nil? d) (subs s 1))
             (recur (conj acc result) (nil? d) s)))
-        (let [[d s] (parse-do s)
+        (let [[d s] (or (parse-do s)
+                        [nil s])
               next-s (if (nil? d) (subs s 1) s)]
           (recur acc (not (nil? d)) next-s)))))
 
