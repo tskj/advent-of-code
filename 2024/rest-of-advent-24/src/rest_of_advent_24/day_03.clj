3dae15a10ef1c4ac5aaab63403de885c5d6c1554
tskj <epost@tarjei.org>
2024-12-05 02:13:23 +0100

    formatting

--- 2024/rest-of-advent-24/src/rest_of_advent_24/day_03.clj
+++ 2024/rest-of-advent-24/src/rest_of_advent_24/day_03.clj
@@ -10,8 +10,7 @@
    (cond (and (seq? line-of-code) (= 'const (first line-of-code))) ;; const
          (do (assert (= 3 (count line-of-code)))
              (let [[_ lhs rhs] line-of-code]
-               `(let [rhs# ~rhs
-                      ~lhs rhs#]
+               `(let [~lhs ~rhs]
                  (blk ~@rest-of-codeblock))))
 
          (and (seq? line-of-code) (= 'const-try (first line-of-code)))
@@ -31,36 +30,6 @@
               (:return result#)
               result#))))))
 
-(let [result__6860__auto__ "test"]
-  (println result__6860__auto__)
-
-  (blk (if (= (+ 1 1) 4) "yeah")
-     (const a "hello")
-     (println (str "hello___" result__6860__auto__))
-     (if true (str "her " a))
-     (if false "return this? no")
-     a))
-
-(macroexpand
-  '(blk (const a "hello")
-        (if false "return this? no")
-        "hello"))
-
-(blk (const a "hello")
-     (if false "return this? no")
-     "hello")
-
-(blk (if (nil? "") "yeah")
-     (if true {:return "test"})
-     (const a nil)
-     (if true "return this? no")
-     a)
-
-(blk (if (nil? "") "yeah")
-     (const-try a "hei")
-     (if false "return this? no")
-     a)
-
 (def input (slurp "resources/day-03-input.txt"))
 
 (defn is-digit? [d]
@@ -71,15 +40,15 @@
    (if (empty? s) {:return nil})
 
    (const first-character (first s))
-   (const r (subs s 1))
+   (const s (subs s 1))
 
    (if (not (is-digit? first-character)) {:return nil})
 
    (const digit first-character)
-   (const [next-digits s'] (or (parse-n-digit-int (subs s 1))
-                               [nil r]))
+   (const [next-digits s] (or (parse-n-digit-int s)
+                              [nil s]))
 
-   [(str digit (or next-digits "")) s']))
+   [(str digit (or next-digits "")) s]))
 
 (defn str-to-int [s]
   (assert (every? is-digit? s) "this function only applies to int digits")
@@ -89,61 +58,58 @@
                          (+ digit (* 10 acc)))))))
 
 (defn parse-literal [p s]
-  (assert (not (nil? s)) "string can't be null")
-  (if (starts-with? s p)
+  (when (starts-with? s p)
     [p (subs s (count p))]))
 
 (defn parse-comma [s]
-  (assert (not (nil? s)) "comma s is nil")
   (parse-literal "," s))
 
 (defn parse-ending-paren [s]
-  (assert (not (nil? s)) "end-paren s is nil")
   (parse-literal ")" s))
 
 (defn parse-do [s]
-  (assert (not (nil? s)) "do s is nil")
   (parse-literal "do()" s))
 
 (defn parse-don't [s]
-  (assert (not (nil? s)) "don't s is nil")
   (parse-literal "don't()" s))
 
-(def mul "mul(") ;; )
+(defn parse-mul [s]
+  (parse-literal "mul(" s))
 
 (defn parse-mul-thing [initial-s]
   (blk
-    (const-try [_mul         s] (parse-literal mul initial-s))
-    (const-try [the-int      s] (parse-n-digit-int s))
-    (const-try [_comma       s] (parse-comma s))
-    (const-try [the-next-int s] (parse-n-digit-int s))
-    (const-try [_end-paren   s] (parse-ending-paren s))
+    (const-try [_mul        s] (parse-mul initial-s))
+    (const-try [first-int   s] (parse-n-digit-int s))
+    (const-try [_comma      s] (parse-comma s))
+    (const-try [second-int  s] (parse-n-digit-int s))
+    (const-try [_end-paren  s] (parse-ending-paren s))
 
-    [(* (str-to-int the-int) (str-to-int the-next-int)) s]))
+    [(* (str-to-int first-int) (str-to-int second-int)) s]))
 
 (->>
 
   (loop [acc []
-         enabled true
+         enabled? true
          s input]
 
-    (cond (empty? s)
-          acc
+    (cond
+      (empty? s)
+      acc
 
-          enabled
-          (let [[result s] (or (parse-mul-thing s)
-                               [nil s])
-                [d s] (or (parse-don't s)
-                          [nil s])]
-            (if (nil? result)
-              (recur acc (nil? d) (subs s 1))
-              (recur (conj acc result) (nil? d) s)))
+      enabled?
+      (let [[result s] (or (parse-mul-thing s)
+                           [nil s])
+            [d      s] (or (parse-don't s)
+                           [nil s])]
+        (if (nil? result)
+          (recur acc (nil? d) (subs s 1))
+          (recur (conj acc result) (nil? d) s)))
 
-          :else
-          (let [[d s] (or (parse-do s)
-                          [nil s])
-                next-s (if (nil? d) (subs s 1) s)]
-            (recur acc (not (nil? d)) next-s))))
+      :else
+      (let [[d s] (or (parse-do s)
+                      [nil s])
+            next-s (if (nil? d) (subs s 1) s)]
+        (recur acc (not (nil? d)) next-s))))
 
   (reduce +))
 
