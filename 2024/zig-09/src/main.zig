257425d28802b6f35c7fbe28287aaff5b1be6411
tskj <epost@tarjei.org>
2024-12-10 17:27:05 +0100

    day 9 part 2 zig

--- 2024/zig-09/src/main.zig
+++ 2024/zig-09/src/main.zig
@@ -73,23 +73,55 @@ pub fn main() !void {
         // find next free
         while (free_offset < memory.len) : (free_offset += 1) {
             if (memory[free_offset] == -1) break;
-        } else unreachable;
+        } else break;
 
         // find last used
         while (end_offset > 0) : (end_offset -= 1) {
             if (memory[end_offset] != -1) break;
-        } else unreachable;
+        } else break;
 
         if (free_offset > end_offset) break;
 
-        swap(memory, free_offset, end_offset);
+        const file_id = memory[end_offset];
+        var start_offset = end_offset;
+        while (memory[start_offset] == file_id) : (start_offset -= 1) {}
+        assert(free_offset <= start_offset);
+
+        const block_size = end_offset - start_offset;
+        assert(block_size >= 1);
+
+        var this_free_offset = free_offset;
+        while (true) {
+            const l = getLengthOfFreeBlock(memory, this_free_offset);
+            if (l >= block_size) break;
+
+            // find next free
+            while (this_free_offset < memory.len) : (this_free_offset += 1) {
+                if (memory[this_free_offset] != -1) break;
+            } else break;
+            while (this_free_offset < memory.len) : (this_free_offset += 1) {
+                if (memory[this_free_offset] == -1) break;
+            } else break;
+        }
+
+        if (this_free_offset < memory.len and this_free_offset < end_offset) {
+            const block_id = memory[end_offset];
+            fill(memory, this_free_offset, block_size, block_id);
+            fill(memory, start_offset + 1, block_size, -1);
+        } else {
+            // can't be moved, let's find next block
+            const block_id = memory[end_offset];
+            while (end_offset > 0) : (end_offset -= 1) {
+                if (memory[end_offset] != block_id and memory[end_offset] != -1) break;
+            }
+        }
     }
 
-    // std.debug.print("\n\n", .{});
-    // for (memory) |b| {
-    //     if (b >= 0) std.debug.print("|{d}|", .{b}) else std.debug.print(".", .{});
-    // }
-    // std.debug.print("\n\n", .{});
+    std.debug.print("\n\n", .{});
+    for (memory) |b| {
+        if (b >= 0) std.debug.print("|{d}|", .{b}) else std.debug.print(".", .{});
+    }
+    std.debug.print("\n\n", .{});
 
     var check_sum: usize = 0;
     for (memory, 0..) |b, j| {
@@ -97,6 +129,24 @@ pub fn main() !void {
     }
 
     std.debug.print("checksum is: {d}\n", .{check_sum});
+}
+
+fn fill(m: []i32, offset: usize, n: usize, x: i32) void {
+    var i: usize = 0;
+    while (offset + i < m.len and i < n) : (i += 1) {
+        m[offset + i] = x;
+    }
+}
+
+fn getLengthOfFreeBlock(m: []i32, offset: usize) usize {
+    assert(m[offset] == -1);
+    // find next non-free
+    var next = offset;
+    while (next < m.len) : (next += 1) {
+        if (m[next] != -1) break;
+    }
+
+    return next - offset;
 }
 
 fn swap(m: []i32, a: usize, b: usize) void {
