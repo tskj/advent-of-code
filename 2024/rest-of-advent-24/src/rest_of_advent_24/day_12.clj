738a8f5ed5ea6f0236af570af88f0669b26ee010
tskj <epost@tarjei.org>
2024-12-12 15:05:53 +0100

    piss

--- 2024/rest-of-advent-24/src/rest_of_advent_24/day_12.clj
+++ 2024/rest-of-advent-24/src/rest_of_advent_24/day_12.clj
@@ -49,8 +49,8 @@
        (map create-region)
        (into #{})))
 
-; (->> regions
-;      (group-by (comp get-plant first)))
+(->> regions
+     (group-by (comp get-plant first)))
 
 (defn edges [c]
   [#{c              (add c [0 1])}
@@ -58,11 +58,43 @@
    #{(add c [0 1])  (add c [1 1])}
    #{(add c [1 0])  (add c [1 1])}])
 
+(defn join-edge [e1 e2]
+  (let [all-corners (concat e1 e2)]
+    (->> all-corners
+        (filter (fn [corner] (= 1 (count (filter #(= % corner) all-corners)))))
+        (into #{}))))
+
+(defn share-corner? [e1 e2]
+  (let [all-corners (into #{} (concat e1 e2))]
+    (not= (count all-corners)
+          4)))
+
+(defn orientation [edge]
+  (let [[y x] (first edge)
+        [a b] (second edge)]
+    (if (= (- y a) 0)
+      :horizontal
+      :vertical)))
+
+
+(defn join-edges [edges]
+  (->> edges
+       (reduce (fn [acc edge]
+                  (let [e (->> acc (some (fn [e] (and (= (orientation e) (orientation edge))
+                                                      (share-corner? e edge)
+                                                      e))))
+                        acc-without-e (->> acc (filter #(not= % e)))]
+                    (if e
+                      (conj acc-without-e (join-edge e edge))
+                      (conj acc edge))))
+               [])))
+
 (defn calc-area [region] (count region))
 (defn calc-perimeter [region]
   (let [edges-for-each-plant (->> region (mapcat edges))
         appears-only-once?   (fn [edge] (= 1 (count (filter #(= edge %) edges-for-each-plant))))
-        perimeter            (->> edges-for-each-plant (filter appears-only-once?))]
+        perimeter            (->> edges-for-each-plant (filter appears-only-once?)
+                                                       (join-edges))]
     (count perimeter)))
 
 (->> regions
