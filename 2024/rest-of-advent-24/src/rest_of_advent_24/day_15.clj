978352ba59d7487416c762db3ac83a15936aa8d4
tskj <epost@tarjei.org>
2024-12-15 16:28:04 +0100

    day 15 part 1

--- 2024/rest-of-advent-24/src/rest_of_advent_24/day_15.clj
+++ 2024/rest-of-advent-24/src/rest_of_advent_24/day_15.clj
@@ -4,7 +4,7 @@
    [rest-of-advent-24.utils.macros :refer [blk]]))
 
 (def input
-  (->> (slurp "resources/test/day-15-input.txt")
+  (->> (slurp "resources/day-15-input.txt")
        (#(split % #"\n\n"))))
 
 (def warehouse
@@ -27,7 +27,7 @@
   (let [robot (atom nil)]
     (doseq [y (range height)]
       (doseq [x (range width)]
-        (when (= \@ (get-in warehouse [y x]))
+        (when (= \@ (get-in m [y x]))
           (reset! robot [y x]))))
     (assert (some? @robot))
     @robot))
@@ -56,6 +56,9 @@
 (def right  [0 1])
 (def left   [0 -1])
 
+(defn rev-dir [[y x]]
+  [(* -1 y) (* -1 x)])
+
 (defn add [[a b] [c d]]
   [(+ a c) (+ b d)])
 
@@ -82,12 +85,38 @@
       (const end-coord
              (loop [new-p potential]
                (if (is-box? m new-p)
-                   (recur (add new-p dir))
+                   (recur (add new-p d))
                    (if (or (is-empty? m new-p) (is-wall? m new-p))
                        new-p
                        (assert false "reached end of boxes but not wall or empty somehow")))))
-      (if (is-wall? m end-coord) (return m)) 
-
+      (if (is-wall? m end-coord) (return m))
       (assert (is-empty? m end-coord))
-      (loop [m' m])))) 
-              
+
+      (const backwards (rev-dir d))
+      (loop [m' m
+             c end-coord]
+        (assert (is-empty? m' c))
+        (if (is-robot? m' (add c d))
+            m'
+            (let [to-swap (add c backwards)]
+              (assert (or (is-box? m' to-swap)
+                          (is-robot? m' to-swap)))
+              (recur (swp m' c to-swap)
+                     to-swap)))))))
+
+(def final-warehous
+  (->> robot-movements
+       (reduce step warehouse)))
+
+(defn gps-of-coord [[y x]]
+  (+ (* y 100) x))
+
+
+(let [box-coords (atom #{})]
+  (doseq [y (range height)]
+    (doseq [x (range width)]
+      (when (is-box? final-warehous [y x])
+        (swap! box-coords conj [y x]))))
+  (->> @box-coords 
+       (map gps-of-coord)
+       (reduce +))) 
