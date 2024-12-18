2b0f4029c105cabd5ee0a411eea4e88bd7a88881
tskj <epost@tarjei.org>
2024-12-18 15:14:45 +0100

    part 2 day 18

--- 2024/rest-of-advent-24/src/rest_of_advent_24/day_17.clj
+++ 2024/rest-of-advent-24/src/rest_of_advent_24/day_17.clj
@@ -5,7 +5,7 @@
    [rest-of-advent-24.utils.macros :refer [blk]]))
 
 (def input-program
-  (->> (slurp "resources/test/day-17-input.txt")))
+  (->> (slurp "resources/day-17-input.txt")))
 
 (defn parse-literal [literal s]
   (blk
@@ -84,7 +84,7 @@
          p 1]
     (if (<= x 0)
        p
-       (recur (dec x) (* p 2)))))
+       (recur (dec' x) (*' p 2)))))
 
 (defn eval-combo [computer operand]
   (case operand
@@ -105,12 +105,12 @@
     (case opcode
       0         (let [numerator (->> computer :cpu :a)
                       denominator (->> (power-of-2 (eval-combo computer operand)))]
-                  (->> (int (/ numerator denominator))
+                  (->> (bigint (/ numerator denominator))
                        (assoc-in computer [:cpu :a])
                        (advptr)))
 
       1         (let [b (->> computer :cpu :b)]
-                  (->> (bit-xor b operand)
+                  (->> (bit-xor (long b) operand)
                        (assoc-in computer [:cpu :b])
                        (advptr)))
 
@@ -128,7 +128,7 @@
 
       4         (let [b (->> computer :cpu :b)
                       c (->> computer :cpu :c)]
-                  (->> (bit-xor b c)
+                  (->> (bit-xor (long b) (long c))
                        (assoc-in computer [:cpu :b])
                        (advptr)))
 
@@ -139,13 +139,13 @@
 
       6         (let [numerator (->> computer :cpu :a)
                       denominator (->> (power-of-2 (eval-combo computer operand)))]
-                  (->> (int (/ numerator denominator))
+                  (->> (bigint (/ numerator denominator))
                        (assoc-in computer [:cpu :b])
                        (advptr)))
 
       7         (let [numerator (->> computer :cpu :a)
                       denominator (->> (power-of-2 (eval-combo computer operand)))]
-                  (->> (int (/ numerator denominator))
+                  (->> (bigint (/ numerator denominator))
                        (assoc-in computer [:cpu :c])
                        (advptr))))))
 
@@ -173,68 +173,241 @@
 (def number-of-cache-hits (atom 0))
 (def number-of-cache-misses (atom 0))
 
-(defn run-computer [computer]
-  (let [past-states (atom {(:cpu computer) computer})]
-    (loop [c computer]
-      (if-let [c* (read-lru (:cpu c))]
-        (let [c** (-> c* (assoc :output (vec (concat (:output c) (:output c*)))))]
-          (doseq [[k v] @past-states]
-             (let [old-output (vec (:output v))
-                   new-output (vec (:output c**))
-                   actual-output (vec (drop (count old-output) new-output))]
-               (assert (= old-output (subvec new-output 0 (count old-output))))
-               (write-lru k (assoc v :output actual-output))))
-
-          (write-lru (:cpu computer) c**)
-          (swap! number-of-cache-hits inc)
-          c**)
-
-        (let [nc (step c)]
-          (if (@past-states (:cpu nc))
-            (do
-              (assert false "loop detetcted sadge")
-              computer)
-            (do
-              (swap! past-states assoc (:cpu nc) nc)
-              (if (has-halted? nc)
-                (do
-                  ; (->> (vec @past-states)
-                  ;      (mapv (fn [k] [k nc]))
-                  ;      (into {})
-                       (doseq [[k v] @past-states]
-                          (let [old-output (vec (:output v))
-                                new-output (vec (:output nc))
-                                actual-output (vec (drop (count old-output) new-output))]
-                            (assert (= old-output (subvec new-output 0 (count old-output))))
-                            (write-lru k (assoc v :output actual-output))))
-                       (write-lru (:cpu computer) nc)
-                       (swap! number-of-cache-misses inc)
-                  nc)
-                (recur nc)))))))))
+(defn starts-with?v [main-v sub-v]
+  (= sub-v (subvec main-v 0 (count sub-v))))
 
 (def corrupted-computer (parse-program input-program))
 
-(def computer (loop [candidate-a 0]
-                (when (= (mod candidate-a 100000) 0)
-                  (println "jobber på kandidat " candidate-a)
-                  (println "lengde på cache    " (count @previous-state))
-                  (println "lru queue size     " (count @recently-used) (count (set @recently-used)))
-                  (println "antall cache hits  " @number-of-cache-hits)
-                  (println "antall cache misses" @number-of-cache-misses)
-                  (println "totalt:            " (+ @number-of-cache-hits @number-of-cache-misses)))
-                (let [end-computer (run-computer (assoc-in corrupted-computer [:cpu :a] candidate-a))]
-                  (if (= (:output end-computer)
-                         (:memory corrupted-computer))
-                      (do
-                        (println "result is " candidate-a)
-                        end-computer)
-                      (recur (inc candidate-a))))))
-computer
+(defn run-computer [computer]
+  (let [past-states (atom {(:cpu computer) computer})]
+    (loop [c computer]
+      (if-not (starts-with?v (:output c) (:output corrupted-computer))
+        (let [borked-computer (assoc c :output [])]
+          borked-computer)
+        (if-let [c* (read-lru (:cpu c))]
+          (let [c** (-> c* (assoc :output (vec (concat (:output c) (:output c*)))))]
+            (doseq [[k v] @past-states]
+               (let [old-output (vec (:output v))
+                     new-output (vec (:output c**))
+                     actual-output (vec (drop (count old-output) new-output))]
+                 (assert (= old-output (subvec new-output 0 (count old-output))))
+                 (write-lru k (assoc v :output actual-output))))
 
-(->>
-  computer
-  :output
-  (join ","))
+            (write-lru (:cpu computer) c**)
+            (swap! number-of-cache-hits inc)
+            c**)
+
+          (let [nc (step c)]
+            (if (@past-states (:cpu nc))
+              (do
+                (assert false "loop detetcted sadge")
+                computer)
+              (do
+                (swap! past-states assoc (:cpu nc) nc)
+                (if (has-halted? nc)
+                  (do
+                    ; (->> (vec @past-states)
+                    ;      (mapv (fn [k] [k nc]))
+                    ;      (into {})
+                         (doseq [[k v] @past-states]
+                            (let [old-output (vec (:output v))
+                                  new-output (vec (:output nc))
+                                  actual-output (vec (drop (count old-output) new-output))]
+                              (assert (= old-output (subvec new-output 0 (count old-output))))
+                              (write-lru k (assoc v :output actual-output))))
+                         (write-lru (:cpu computer) nc)
+                         (swap! number-of-cache-misses inc)
+                    nc)
+                  (recur nc))))))))))
+
+
+(defn run-computer-until-first-output [computer]
+    (let [start-output (:output computer)]
+      (loop [c computer]
+        (if (has-halted? c)
+          c
+          (let [next (step c)
+                output (:output next)]
+            (if (> (count output) (count start-output))
+                next
+                (recur next)))))))
+
+; a'%8 -> b'''
+; b'''^3 -> b''
+; a'/2**b'' -> c''
+; a'/2**3 -> a
+; b''^c'' -> b'
+; b'^5 -> b
+; b%8 -> output
+; a!=0? loop
+
+
+(defn backwards* [a o]
+  (->> (range 0 8)
+       (mapcat
+         (fn [n]
+           (let [a' (+ (* a 8) n)]
+              (assert (= a (bigint (/ a' 8))))
+              (let [b''' (mod a' 8)
+                    b'' (bit-xor (long b''') 3)
+                    c'' (bigint (/ a' (power-of-2 b'')))
+                    b' (bit-xor (long b'') (long c''))
+                    b (bit-xor (long b') 5)]
+                (if (= (mod b 8) o)
+                    [a']
+                    [])))))))
+
+(time
+  (loop [as [(bigint 0)]
+         memory (reverse (:memory corrupted-computer))]
+    (if (empty? memory) 
+      as
+      (let [current-m (first memory)] 
+        (recur (mapcat (fn [a] (backwards* a current-m)) as)
+               (rest memory)))))) 
+
+; 236581108670061
+; 236581108670143
+
+; a o er "input"
+; b = o, o+8, o+16, o+24, ...
+; b' = b^5
+
+; a* = a - (mod a 8)
+; oops wrong, flip addition and multiplication step:
+; a' = a* * 8, (a*+1) * 8, (a*+2) * 8, ... (a*+7) * 8
+; eksempel: a = 6
+;           48 / 8 -> 6
+;           49 / 8 -> 6
+;           50 / 8 -> 6
+;           ...
+;           55 / 8 -> 6
+; (assert (int a'/8) == a)
+
+; b''' = (mod a' 8)
+; b'' = b'''^3
+; c'' = (int a'/2**b'')
+; b' = b''^c''
+; b = b'^5
+; does (mod b 8) == o?
+
+
+; c'' = b'^b''
+
+; assert c'' == a' * 2**b''
+; assert (int a'/2**b'') == c''
+
+
+(defn teest [a]
+  (blk
+    (const b (mod a 8))
+    (const b (bit-xor (long b) 3))
+    (const c (bigint (/ a (power-of-2 b))))
+    (const a (bigint (/ a (power-of-2 3))))
+    (const b (bit-xor (long b) (long c)))
+    (const b (bit-xor (long b) 5))
+    (const o (mod b 8))
+    [a o]))
+
+(teest 1)
+(teest 2)
+(teest 3)
+(teest 4)
+(teest 5)
+(= (teest 6) [0 0])
+(teest 7)
+
+
+(defn arg-eql [f args output]
+  (if (empty? args)
+    (assert false (str "no argument produces output: " output))
+    (let [arg (first args)]
+      (if (= (f arg) output)
+        arg
+        (recur f (rest args) output)))))
+
+(->> (range 0 199)
+     (#(arg-eql teest % [6 3])))
+
+(arg-eql teest (map (partial + 0) (range 0 8)) [0 0])
+
+(loop [backwards-memory (reverse (:memory corrupted-computer))
+       a 0]
+  (println a backwards-memory)
+  (let [current-mem (first backwards-memory)]
+    (if-not current-mem
+      a
+      (recur (rest backwards-memory)
+             (arg-eql teest (map (partial + a) (range)) [a current-mem])))))
+
+
+; a = 6
+; b = 0b110 (6)
+; 3 = 0b011
+; 6 / 64 -> c = 0
+; a == 6
+; a = 0
+; c == 0
+; b = 5 (6^0)
+; 5 = 0b101
+; b == 0
+
+; 2,4,1,3,7,5,0,3,4,1,1,5,5,5,3,0
+
+
+; (run-computer-until-first-output (assoc-in corrupted-computer [:cpu :a] 117440))
+
+; (->> (range 0 100000)
+;      (map (fn [a] [a (run-computer-until-first-output
+;                        (run-computer-until-first-output
+;                          (run-computer-until-first-output
+;                            (run-computer-until-first-output
+;                              (run-computer-until-first-output (assoc-in corrupted-computer [:cpu :a] a))))))]))
+;      (filter (fn [[a c]] (<= 3 (count (:output c)))))
+;      (filter (fn [[a c]] (= (subvec (:output c) 0 3)
+;                             (subvec (:memory c) 0 3))))
+;      (map (fn [[a c]] [a (mod a 8) c])))
+
+; (time
+;   (->>
+;     (range 100000000 200000000)
+;     (map (fn [a] [a (run-computer-until-first-output (assoc-in corrupted-computer [:cpu :a] a))]))
+;     (filter (fn [[a c]] (= (first (:output c)) (first (:memory corrupted-computer)))))
+;     (map (fn [[a c]] [a (run-computer-until-first-output c)]))
+;     (filter (fn [[a c]] (= (second (:output c)) (second (:memory corrupted-computer)))))
+;     (map (fn [[a c]] [a (run-computer-until-first-output c)]))
+;     (filter (fn [[a c]] (= (first (drop 2 (:output c))) (first (drop 2 (:memory corrupted-computer))))))
+;     (keep (fn [[a c]] (let [end-computer (run-computer c)]
+;                          (when (= (:output end-computer)
+;                                   (:memory corrupted-computer))
+;                            (println "result is !!! " a)
+;                            [a end-computer]))))))
+
+(def start (power-of-2 (bigint 48)))
+
+; (def computer (loop [candidate-a (dec (power-of-2 (bigint 48)))]
+;                 (when (= (mod candidate-a 10000) 0)
+;                   (println "jobber på kandidat " candidate-a)
+;                   (println "siden start        " (- candidate-a start))
+;                   (println "lengde på cache    " (count @previous-state))
+;                   (println "lru queue size     " (count @recently-used) (count (set @recently-used)))
+;                   (println "antall cache hits  " @number-of-cache-hits)
+;                   (println "antall cache misses" @number-of-cache-misses)
+;                   (println "totalt:            " (+ @number-of-cache-hits @number-of-cache-misses)))
+;                 (let [end-computer (run-computer (assoc-in corrupted-computer [:cpu :a] candidate-a))]
+;                   (assert false)
+;                   (if (= (:output end-computer)
+;                          (:memory corrupted-computer))
+;                       (do
+;                         (println "result is " candidate-a)
+;                         end-computer)
+;                       (recur (inc candidate-a))))))
+; computer
+
+; (->>
+;   computer
+;   :output
+;   (join ","))
 
 
 
