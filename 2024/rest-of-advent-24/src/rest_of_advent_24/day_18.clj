201606ecfada41abac1acfec5e86786e6459d412
tskj <epost@tarjei.org>
2024-12-18 22:42:29 +0100

    something

---
+++ 2024/rest-of-advent-24/src/rest_of_advent_24/day_18.clj
@@ -0,0 +1,98 @@
+(ns rest-of-advent-24.day-18
+  (:require
+   [clojure.string :refer [join split split-lines]]))
+
+(def file
+  (->> (slurp "resources/test/day-18-input.txt")
+       (split-lines)))
+
+(def number-of-bytes-to-simulate
+  (parse-long (first file)))
+
+(def dimensions
+  (mapv parse-long (split (second file) #"x")))
+
+(def width (inc (first dimensions)))
+(def height (inc (second dimensions)))
+
+(def target [(dec width) (dec height)])
+
+(def incoming-bytes
+  (->> file
+       (rest)
+       (rest)
+       (take number-of-bytes-to-simulate)
+       (mapv #(split % #","))
+       (mapv #(mapv parse-long %))))
+
+(def walls incoming-bytes)
+
+(def empty-map
+  (-> (vec (repeat height (vec (repeat width \.))))))
+
+(defn render [mmm things pencil]
+  (->> things
+       (mapv (fn [[x y]] [y x]))
+       (reduce (fn [map' thing] (assoc-in map' thing pencil)) mmm)))
+
+(defn draw [map*]
+  (println (join "\n" (mapv join map*))))
+
+(defn h [n]
+  (let [[x y] n
+        [x* y*] target]
+    (+ (- x* x)
+       (- y* y))))
+
+(defn advance-sloth-up [sloth]
+  (-> sloth
+      (update-in [:p 1] dec)
+      (update-in [:g] inc)
+      (update-in [:path] conj (:p sloth))))
+
+(defn advance-sloth-right [sloth]
+  (-> sloth
+      (update-in [:p 0] inc)
+      (update-in [:g] inc)
+      (update-in [:path] conj (:p sloth))))
+
+(defn advance-sloth-down [sloth]
+  (-> sloth
+      (update-in [:p 1] inc)
+      (update-in [:g] inc)
+      (update-in [:path] conj (:p sloth))))
+
+(defn advance-sloth-left [sloth]
+  (-> sloth
+      (update-in [:p 0] dec)
+      (update-in [:g] inc)
+      (update-in [:path] conj (:p sloth))))
+
+(defn n-for-sloth [sloth]
+  (->>
+    [advance-sloth-up advance-sloth-right advance-sloth-down advance-sloth-left]
+    (mapv (fn [f] (f sloth)))))
+
+(defn f [sloth]
+  (+ (:g sloth) (h (:p sloth))))
+
+(def visited-nodes (atom #{[0 0]}))
+
+(defn is-finished? [sloth]
+  (when (= target (:p sloth))
+    sloth))
+
+(->
+  (loop [sloths [{:g 0 :p [0 0] :path []}]]
+    (if-let [finish (some is-finished? sloths)] finish
+      (let [p-sloth (apply min-key f sloths)
+            new-sloths (->> (n-for-sloth p-sloth)
+                            (remove (fn [sloth] (@visited-nodes (:p sloth)))))]
+        (swap! visited-nodes into (map :p new-sloths))
+        (recur (->> sloths
+                    (remove (fn [sloth] (= (:p p-sloth) (:p sloth))))
+                    (concat new-sloths))))))
+  (#(conj (:path %) (:p %)))
+  (#(render empty-map % \O))
+  (render walls \#)
+  (draw))
