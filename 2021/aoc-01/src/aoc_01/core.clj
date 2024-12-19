e7c09b2acbe26ca0afa0d92bca41da131754696b
tskj <epost@tarjei.org>
2024-12-19 13:37:08 +0100

    lanternfish day 6 part 2 year 2021

--- 2021/aoc-01/src/aoc_01/core.clj
+++ 2021/aoc-01/src/aoc_01/core.clj
@@ -6,26 +6,33 @@
 ;; (def input-file "./data/input.txt")
 
 (def initial-fish
-  (->> input-file
-       (slurp)
+  (->> "2,1,1,4,4,1,3,4,2,4,2,1,1,4,3,5,1,1,5,1,1,5,4,5,4,1,5,1,3,1,4,2,3,2,1,2,5,5,2,3,1,2,3,3,1,4,3,1,1,1,1,5,2,1,1,1,5,3,3,2,1,4,1,1,1,3,1,1,5,5,1,4,4,4,4,5,1,5,1,1,5,5,2,2,5,4,1,5,4,1,4,1,1,1,1,5,3,2,4,1,1,1,4,4,1,2,1,1,5,2,1,1,1,4,4,4,4,3,3,1,1,5,1,5,2,1,4,1,2,4,4,4,4,2,2,2,4,4,4,2,1,5,5,2,1,1,1,4,4,1,4,2,3,3,3,3,3,5,4,1,5,1,4,5,5,1,1,1,4,1,2,4,4,1,2,3,3,3,3,5,1,4,2,5,5,2,1,1,1,1,3,3,1,1,2,3,2,5,4,2,1,1,2,2,2,1,3,1,5,4,1,1,5,3,3,2,2,3,1,1,1,1,2,4,2,2,5,1,2,4,2,1,1,3,2,5,5,3,1,3,3,1,4,1,1,5,5,1,5,4,1,1,1,1,2,3,3,1,2,3,1,5,1,3,1,1,3,1,1,1,1,1,1,5,1,1,5,5,2,1,1,5,2,4,5,5,1,1,5,1,5,5,1,1,3,3,1,1,3,1"
        (#(str/split % #","))
        (map #(Integer/parseInt %))))
 
-(defn update-fish [fish]
-  (if (<= fish 0)
-    [6 8]
-    [(dec fish)]))
+(defn update-fish [[fish count]]
+  (assert (not (neg? fish)))
+  (if (= fish 0)
+    [[6 count] [8 count]]
+    [[(dec fish) count]]))
 
-(def days-to-simulate 256)
+(def days-to-simulate (dec 256))
 
-(def fishies 
-  (loop [fish initial-fish
+(->>
+  (group-by first [[6 3] [8 3] [0 1] [8 1]])
+  (map (fn [[k v]] [k (reduce + (map second v))])))
+
+(def fishies
+  (loop [counts (frequencies initial-fish)
          day 0]
-    ;; (print "After " day " days: " fish "\n")
-    (if (<= days-to-simulate day)
-      fish
-      (recur
-       (mapcat update-fish fish)
-       (+ 1 day)))))
+    (let [updated-fishies (->> (mapcat update-fish counts)
+                               (group-by first)
+                               (map (fn [[k v]] [k (reduce + (map second v))]))
+                               (into {}))]
+      (if (<= days-to-simulate day)
+        updated-fishies
+        (recur
+         updated-fishies
+         (inc day))))))
 
-(count fishies)
+(reduce + (map second fishies))
